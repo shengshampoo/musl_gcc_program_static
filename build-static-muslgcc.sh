bf96c9f302bedc8fdb823c0e7781f14b71e4c2c1
shengshampoo <shengzh@tutanota.com>
2025-09-12 11:21:37 +0800

    Update build-static-muslgcc.sh

--- build-static-muslgcc.sh
+++ build-static-muslgcc.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-WORKSPACE=/tmp/workspace2
+WORKSPACE=/tmp/workspace
 mkdir -p $WORKSPACE
 mkdir -p /work/artifact
 
@@ -11,6 +11,18 @@ mkdir -p /work/artifact
 cd $WORKSPACE
 git clone https://github.com/richfelker/musl-cross-make
 cd musl-cross-make
-mv ../config.mak.3.musl config.mak
+cp ../config.mak.3.musl config.mak
 make
 make install
+
+mv /usr/local/$(uname -m)-linux-musl-cross-stage0/$(uname -m)-linux-musl/lib/libstdc++.so /usr/local/$(uname -m)-linux-musl-cross-stage0/$(uname -m)-linux-musl/lib/libstdc++.so.old
+ln -sf /usr/local/$(uname -m)-linux-musl-cross-stage0/$(uname -m)-linux-musl/lib/libstdc++.a /usr/local/$(uname -m)-linux-musl-cross-stage0/$(uname -m)-linux-musl/lib/libstdc++.so
+make clean
+cp ../config.mak.2.musl config.mak
+make
+make install
+
+cd /usr/local
+tar vcJf ./$(uname -m)-linux-musl-cross.tar.xz $(uname -m)-linux-musl-cross
+
+mv ./$(uname -m)-linux-musl-cross.tar.xz /work/artifact/
