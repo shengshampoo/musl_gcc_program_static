ace0c447c3285cd5bdb8ed8cca04e30bfc88f214
shengshampoo <shengzh@tutanota.com>
2025-09-12 11:27:52 +0800

    Update build-static-muslgcc.sh

--- build-static-muslgcc.sh
+++ build-static-muslgcc.sh
@@ -11,14 +11,14 @@ mkdir -p /work/artifact
 cd $WORKSPACE
 git clone https://github.com/richfelker/musl-cross-make
 cd musl-cross-make
-cp ../config.mak.3.musl config.mak
+cp /config.mak.3.musl config.mak
 make
 make install
 
 mv /usr/local/$(uname -m)-linux-musl-cross-stage0/$(uname -m)-linux-musl/lib/libstdc++.so /usr/local/$(uname -m)-linux-musl-cross-stage0/$(uname -m)-linux-musl/lib/libstdc++.so.old
 ln -sf /usr/local/$(uname -m)-linux-musl-cross-stage0/$(uname -m)-linux-musl/lib/libstdc++.a /usr/local/$(uname -m)-linux-musl-cross-stage0/$(uname -m)-linux-musl/lib/libstdc++.so
 make clean
-cp ../config.mak.2.musl config.mak
+cp /config.mak.2.musl config.mak
 make
 make install
 
